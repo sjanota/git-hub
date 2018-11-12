92d2fd137d194a6e146b3a060b206df0d18b1639
Szymon Janota <szymon.janota@sap.com>
2018-11-12 18:19:39 +0100

    Fix status

--- pkg/ops/status.go
+++ pkg/ops/status.go
@@ -21,8 +21,8 @@ func Status(repo git.Repo) error {
 	if pr.Comment == "" {
 		fmt.Printf("Pull request %s#%v is in sync with GitHub\n", pr.Remote, pr.Number)
 	} else {
-		fmt.Printf("Pull request %s#%v is not out-of-sync\n", pr.Remote, pr.Number)
-		fmt.Printf(`    (use "git pr push" to push comment to GitHub)\n`)
+		fmt.Printf("Pull request %s#%v is out-of-sync\n", pr.Remote, pr.Number)
+		fmt.Printf("    (use \"git pr push\" to push comment to GitHub)\n")
 		fmt.Println("Comment:")
 		for _, line := range strings.Split(pr.Comment, "\n") {
 			fmt.Printf("    %s\n", line)

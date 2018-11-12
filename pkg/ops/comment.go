5cbfadd7138ac2e1f920213a51b153906b24a599
Szymon Janota <szymon.janota@sap.com>
2018-11-12 15:27:01 +0100

    Protect from missing pull request in comment

--- pkg/ops/comment.go
+++ pkg/ops/comment.go
@@ -1,12 +1,16 @@
 package ops
 
 import (
+	"fmt"
 	"github.com/sjanota/git-hub/pkg/git"
 )
 
 func Comment(repo git.Repo, editor git.CommentEditor) error {
 	pr, err := getPullRequestForCurrentBranch(repo)
-	if err != nil {
+	if _, ok := err.(git.NoPullRequestForBranch); err != nil && ok {
+		fmt.Println("No pull request for current branch")
+		return nil
+	} else if err != nil {
 		return err
 	}
 

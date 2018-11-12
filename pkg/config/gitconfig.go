ef1e0bc9d3cac18f4bd106261ced22b135b71810
Szymon Janota <szymon.janota@sap.com>
2018-11-12 08:27:11 +0100

    Remove PR base options from git config

--- pkg/config/gitconfig.go
+++ pkg/config/gitconfig.go
@@ -13,37 +13,6 @@ type gitConfig struct {
 	repo *git.Repository
 }
 
-func (c *gitConfig) GetPullRequestForBranch(branch string) (*PullRequest, error) {
-	cfg, err := c.repo.Config()
-	if err != nil {
-		return nil, err
-	}
-
-	for _, subsection := range cfg.Raw.Section("github-pr").Subsections {
-		if subsection.Option("headRef") == branch {
-			return readPullRequestFromSubsection(subsection)
-		}
-	}
-	return nil, errors.Errorf("no PR for branch %s", branch)
-}
-
-func readPullRequestFromSubsection(subsection *config.Subsection) (*PullRequest, error) {
-	name := strings.Split(subsection.Name, ":")
-	number, err := strconv.Atoi(name[1])
-	if err != nil {
-		return nil, err
-	}
-	return &PullRequest{
-		HeadRef:  subsection.Option("headRef"),
-		HeadRepo: subsection.Option("headRepo"),
-		BaseRef:  subsection.Option("baseRef"),
-		BaseRepo: subsection.Option("baseRepo"),
-		Number:   number,
-		WebURL:   subsection.Option("webUrl"),
-		Remote:   name[0],
-	}, nil
-}
-
 func (c *gitConfig) GetCurrentBranch() (string, error) {
 	ref, err := c.repo.Head()
 	if err != nil {
@@ -106,8 +75,6 @@ func (c *gitConfig) StorePullRequest(remote string, request *PullRequest) error 
 	cfg.Raw.Section("github-pr").Subsection(subsection).
 		SetOption("headRef", request.HeadRef).
 		SetOption("headRepo", request.HeadRepo).
-		SetOption("baseRef", request.BaseRef).
-		SetOption("baseRepo", request.BaseRepo).
 		SetOption("number", strconv.Itoa(request.Number)).
 		SetOption("webUrl", request.WebURL)
 
@@ -117,4 +84,33 @@ func (c *gitConfig) StorePullRequest(remote string, request *PullRequest) error 
 	}
 
 	return nil
+}
+
+func (c *gitConfig) GetPullRequestForBranch(branch string) (*PullRequest, error) {
+	cfg, err := c.repo.Config()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, subsection := range cfg.Raw.Section("github-pr").Subsections {
+		if subsection.Option("headRef") == branch {
+			return readPullRequestFromSubsection(subsection)
+		}
+	}
+	return nil, errors.Errorf("no PR for branch %s", branch)
+}
+
+func readPullRequestFromSubsection(subsection *config.Subsection) (*PullRequest, error) {
+	name := strings.Split(subsection.Name, ":")
+	number, err := strconv.Atoi(name[1])
+	if err != nil {
+		return nil, err
+	}
+	return &PullRequest{
+		HeadRef:  subsection.Option("headRef"),
+		HeadRepo: subsection.Option("headRepo"),
+		Number:   number,
+		WebURL:   subsection.Option("webUrl"),
+		Remote:   name[0],
+	}, nil
 }
--- pkg/config/model.go
+++ pkg/config/model.go
@@ -3,8 +3,6 @@ package config
 type PullRequest struct {
 	HeadRef  string
 	HeadRepo string
-	BaseRef  string
-	BaseRepo string
 	Number   int
 	WebURL   string
 	Remote   string

13bd42c89633b58d9e2447f845e78501ce0aa8ad
Szymon Janota <szymon.janota@sap.com>
2018-11-13 21:07:12 +0100

    Add --pr and --branch flags to open

--- pkg/cli/git-pr/open.go
+++ pkg/cli/git-pr/open.go
@@ -8,16 +8,33 @@ import (
 
 type open struct {
 	repo git.Repo
+
+	pr     *int
+	branch *string
 }
 
 func (o *open) Configure(app *cli.Cli) {
 	app.Command("open", "Opens pull request page in your default browser", func(cmd *cli.Cmd) {
+		cmd.Spec = "[-p | -b]"
+
+		o.pr = cmd.IntOpt("pr p", 0, "Number of PR to open")
+		o.branch = cmd.StringOpt("branch b", "", "Number of PR to open")
+
 		cmd.Action = o.action
 	})
 }
 
 func (o *open) action() {
-	err := ops.Open(o.repo)
+	var prProvider ops.PullRequestProvider
+	if *o.pr != 0 {
+		prProvider = ops.NumberPullRequestProvider{Repo: o.repo, Number: *o.pr}
+	} else if *o.branch != "" {
+		prProvider = ops.BranchPullRequestProvider{Repo: o.repo, Branch: *o.branch}
+	} else {
+		prProvider = ops.CurrentBranchPullRequestProvider{Repo: o.repo}
+	}
+
+	err := ops.Open(prProvider)
 	if err != nil {
 		panic(err)
 	}
--- pkg/ops/open.go
+++ pkg/ops/open.go
@@ -2,12 +2,13 @@ package ops
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
 	"github.com/sjanota/git-pr/pkg/git"
 	"github.com/sjanota/open-golang/open"
 )
 
-func Open(repo git.Repo) error {
-	pr, err := getPullRequestForCurrentBranch(repo)
+func Open(prProvider PullRequestProvider) error {
+	pr, err := prProvider.Get()
 	if noPrErr, ok := err.(git.NoPullRequestForBranch); ok {
 		fmt.Printf("There is no pull request associated with branch '%s'\n", noPrErr.Branch)
 		fmt.Println("    (use \"git pr fetch\" to get pull request if it exists)")
@@ -23,4 +24,58 @@ func Open(repo git.Repo) error {
 	}
 
 	return nil
+}
+
+type PullRequestProvider interface {
+	Get() (*git.PullRequest, error)
+}
+
+type NumberPullRequestProvider struct {
+	Repo   git.Repo
+	Number int
+}
+
+func (p NumberPullRequestProvider) Get() (*git.PullRequest, error) {
+	prs, err := p.Repo.ListPullRequests()
+	if err != nil {
+		return nil, err
+	}
+
+	matches := make([]*git.PullRequest, 0)
+
+	for _, pr := range prs {
+		if pr.Number == p.Number {
+			matches = append(matches, pr)
+		}
+	}
+
+	if len(matches) == 0 {
+		return nil, git.PullRequestNotFound{Number: p.Number}
+	}
+	if len(matches) > 1 {
+		remotes := make([]string, len(matches))
+		for i, pr := range matches {
+			remotes[i] = pr.Remote
+		}
+		return nil, errors.Errorf("ambiguous PR number found in remotes %+v", remotes)
+	}
+
+	return matches[0], nil
+}
+
+type BranchPullRequestProvider struct {
+	Repo   git.Repo
+	Branch string
+}
+
+func (p BranchPullRequestProvider) Get() (*git.PullRequest, error) {
+	return p.Repo.GetPullRequestForBranch(p.Branch)
+}
+
+type CurrentBranchPullRequestProvider struct {
+	Repo git.Repo
+}
+
+func (p CurrentBranchPullRequestProvider) Get() (*git.PullRequest, error) {
+	return getPullRequestForCurrentBranch(p.Repo)
 }

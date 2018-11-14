0ff9f64a037daaf724477ffcebb710a32a03ac8e
Szymon Janota <szymon.janota@sap.com>
2018-11-14 18:29:19 +0100

    Encode comments so special characters can be used

--- pkg/git/editor.go
+++ pkg/git/editor.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
 func (r *repository) GetDefaultTextEditor() (string, error) {
@@ -23,11 +24,16 @@ func (r *repository) GetDefaultTextEditor() (string, error) {
 
 // Calls 'git repository' because go-git does not resolve global repository
 func getCoreEditorFromConfig() (string, error) {
-	cmd := exec.Command("git", "config", "core.editor")
+	cmd := exec.Command("git", "config", "--get", "core.editor")
 
 	output, err := cmd.CombinedOutput()
 
-	if err != nil {
+	if exit, ok := err.(*exec.ExitError); ok {
+		if status, ok := exit.Sys().(syscall.WaitStatus); ok && status.ExitStatus() == 1 {
+			return "", nil
+		}
+		return "", errors.Wrapf(err, "cannot read git config: %s", output)
+	} else if err != nil {
 		return "", errors.Wrapf(err, "cannot read git config: %s", output)
 	}
 
